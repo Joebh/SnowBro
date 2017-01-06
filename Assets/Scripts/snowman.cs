2131141919c87c090c456df30287fb6b8cf1f6ff
Joebh <JoebhJoebh@gmail.com>
2017-01-06 12:03:13 -0500

    add

--- Assets/Scripts/snowman.cs
+++ Assets/Scripts/snowman.cs
@@ -16,23 +16,39 @@ public class snowman : MonoBehaviour {
     private Rigidbody2D rb2d;
     private Animator anim;
 
+    private int direction;
+
     // Use this for initialization
     void Start () {
         rb2d = GetComponent<Rigidbody2D>();
         rb2d.velocity = Vector2.zero;
         anim = GetComponent<Animator>();
 	}
+
+    public void Left()
+    {
+        direction = -1;
+    }
+    public void Right()
+    {
+        direction = 1;
+    }
+
+    public void DonePressing()
+    {
+        direction = 0;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey("d"))
+        if (direction == 1)
         {
             if (rb2d.velocity.x < 12f)
             {
                 rb2d.AddForce(new Vector2(30f, 0));
             }
         }
-        else if (Input.GetKey("a"))
+        else if (direction == -1)
         {
             if (rb2d.velocity.x > -12f)
             {
