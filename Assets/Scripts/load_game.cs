57ccb074908573ca18836c8823f65d0f68564327
Joebh <JoebhJoebh@gmail.com>
2017-01-06 17:47:15 -0500

    add intro scene

---
+++ Assets/Scripts/load_game.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class load_game : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void LoadGame()
+    {
+        Debug.Log("test");
+        SceneManager.LoadScene("play_game");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
